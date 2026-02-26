a47a4812eda02e6b1ca819b0f03df1e3ec779677
RobWardell87 <r.wardell1987@gmail.com>
2026-02-25 19:52:56 -0500

    Art section complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './Components/Hero'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 import Cocktails from './Components/Cocktails'
 import About from './Components/About'
+import Art from './Components/Art'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   )
 }
